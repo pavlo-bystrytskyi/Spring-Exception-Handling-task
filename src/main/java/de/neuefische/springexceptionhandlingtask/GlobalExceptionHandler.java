6139c9b875898f74e4fb39af3bd9b6cab13b6cbc
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-30 10:40:36 +0200

    Added generic exception handler.

--- src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExceptionHandler.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/GlobalExceptionHandler.java
@@ -10,9 +10,18 @@ import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    public static final String SOMETHING_WENT_WRONG_MESSAGE = "Something went wrong";
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorMessage handleException(NoSuchElementException exception) {
         return new ErrorMessage(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus
+    public ErrorMessage handleAllExceptions(Exception exception) {
+        return new ErrorMessage(SOMETHING_WENT_WRONG_MESSAGE);
     }
 }
