49f6950a9e82bd836f7c14ff264404e466b11b5a
kristijanjakimov <kiko.jakimov@gmail.com>
2024-09-30 10:50:50 +0200

    Record changed into a Class

--- src/main/java/de/neuefische/springexceptionhandlingtask/dto/ErrorMessage.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/dto/ErrorMessage.java
@@ -1,4 +1,22 @@
 package de.neuefische.springexceptionhandlingtask.dto;
 
-public record ErrorMessage(String message) {
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class ErrorMessage {
+    private String message;
+    private Instant timestamp;
+
+    public ErrorMessage(String message) {
+        this.message = message;
+        this.timestamp = Instant.now();
+    }
+
+    public ErrorMessage(String message, Instant timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
 }
